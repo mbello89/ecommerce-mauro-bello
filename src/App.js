a966e8269c9edafdc2b307f0e02af5a69634d609
mbello89 <53487672+mbello89@users.noreply.github.com>
2022-03-15 14:00:04 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbarr from './components/Navbar';
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,37 @@
 import React from 'react';
-import {Navbar,NavDropdown,Nav,Container} from 'react-bootstrap';
+import {Navbar,Nav,Container} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../assets/img/logoVagon.png';
 import CartWidget from './CartWidget';
+import './Navbar.css';
 
 
 function Navbarr() {
     return (
-        <Navbar bg="light" expand="md">
+        <Navbar className='NavbarItems' expand="md">
             <Container>
-            <Navbar.Brand href="#home">
-                <img
-                alt="Logo"
-                src={logo}
-                width="45"
-                height="45"
-                className="d-inline-block align-top"
-                />{' '}
-            </Navbar.Brand>
-            <Navbar.Brand href="#home">Un Vagon de Ideas</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>                
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto container justify-content-end">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Nosotros</Nav.Link>
-                    <NavDropdown title="Productos" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Home & Deco</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Kids</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Kitchen</NavDropdown.Item>
-                    </NavDropdown>
-                    <Nav.Link href="#home">
-                        <CartWidget/>
-                    </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+                <Navbar.Brand href="#home">
+                    <img
+                    alt="Logo"
+                    src={logo}
+                    width="45"
+                    height="45"
+                    className="d-inline-block align-top"
+                    />
+                </Navbar.Brand>
+                <Navbar.Brand href="#home">Un Vagon de Ideas</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>                
+                <Navbar.Collapse id="basic-navbar-nav" className='mt-4'>
+                    <Nav className=" container justify-content-end">
+                        <Nav.Link className='nav-links' href="#home">Home</Nav.Link>
+                        <Nav.Link className='nav-links' href="#link">Nosotros</Nav.Link>
+                        <Nav.Link className='nav-links' href="#link">Productos</Nav.Link>
+                        <Nav.Link className='nav-links' href="#link">Contactanos</Nav.Link>
+                        <Nav.Link className='nav-links' href="#home">
+                            <CartWidget/>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
